619729ba01648c8f0db36426f175c089e047696e
Over2Craft <contact@over2craft.com>
2020-12-12 14:33:05 +0100

    init

---
+++ install/server.install.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = include 'settings.php';
+
+chdir(getcwd() . '/..');
+
+if (isset($config['server_os']['override_with_file']) && isset($config['server_os']['override_with_file']) !== null) {
+    copy($config['server_os']['override_with_file'], $config['server_os']['name']);
+} else {
+
+    switch ($config['server_os']['type']) {
+        case 'paper1.16.4':
+            copy(sprintf('https://papermc.io/api/v1/paper/1.16.4/%s/download', $config['server_os']['build']), getcwd() . '/' . $config['server_os']['name']);
+            break;
+        case 'waterfall':
+            copy(sprintf('https://papermc.io/api/v1/waterfall/1.16/%s/download', $config['server_os']['build']), getcwd() . '/' . $config['server_os']['name']);
+            break;
+    }
+
+}
+
+foreach ($config['find'] as $findItem) {
+    $file = file_get_contents(getcwd() . '/' . $findItem['filename']);
+    $new_file = preg_replace($findItem['stringToFind'], $findItem['replaceWith'], $file);
+    file_put_contents(getcwd() . '/' . $findItem['filename'], $new_file);
+}
+
+chown('*', $config['fiesOwner']);
---
+++ install/settings.php
@@ -0,0 +1,38 @@
+<?php
+
+$sererName = 'set server name here';
+$discordToken = 'set discord token here';
+$serverPort = 'server port here';
+$serverType = 'paper1.16.4';
+$serverBuild = 'latest';
+
+return [
+
+    'server_os' => [
+        'name' => $serverType . '-' .$serverBuild ,
+        'type' => $serverType, // paper1.16.4|waterfall
+        'build' => $serverBuild, // 'latest' or build number
+        'override_with_file' => null
+    ],
+
+    'find' => [
+        [
+            'filename' => 'plugins/DiscordSRV/messages.yml',
+            'stringToFind' => '/server_install_server_name/',
+            'replaceWith' => $sererName
+        ],
+        [
+            'filename' => 'plugins/DiscordSRV/config.yml',
+            'stringToFind' => '/server_install_discord_srv_bot_token/',
+            'replaceWith' => $sererName
+        ],
+        [
+            'filename' => 'server.properties',
+            'stringToFind' => '/server-port=25564/',
+            'replaceWith' => $serverPort
+        ]
+    ],
+
+    'fiesOwner' => 'pterodactyl:pterodactyl',
+  
+];
