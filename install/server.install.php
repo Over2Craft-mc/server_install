d9669c3df0e8ae1ba401f96ada5f88454b77da2e
Over2Craft <contact@over2craft.com>
2020-12-12 14:53:15 +0100

    fix jar extension

--- install/server.install.php
+++ install/server.install.php
@@ -5,15 +5,15 @@ $config = include 'settings.php';
 chdir(getcwd() . '/..');
 
 if (isset($config['server_os']['override_with_file']) && isset($config['server_os']['override_with_file']) !== null) {
-    copy($config['server_os']['override_with_file'], $config['server_os']['name']);
+    copy($config['server_os']['override_with_file'], $config['server_os']['name'] . '.jar');
 } else {
 
     switch ($config['server_os']['type']) {
         case 'paper1.16.4':
-            copy(sprintf('https://papermc.io/api/v1/paper/1.16.4/%s/download', $config['server_os']['build']), getcwd() . '/' . $config['server_os']['name']);
+            copy(sprintf('https://papermc.io/api/v1/paper/1.16.4/%s/download', $config['server_os']['build']), getcwd() . '/' . $config['server_os']['name'] . '.jar');
             break;
         case 'waterfall':
-            copy(sprintf('https://papermc.io/api/v1/waterfall/1.16/%s/download', $config['server_os']['build']), getcwd() . '/' . $config['server_os']['name']);
+            copy(sprintf('https://papermc.io/api/v1/waterfall/1.16/%s/download', $config['server_os']['build']), getcwd() . '/' . $config['server_os']['name'] . '.jar');
             break;
     }
 
